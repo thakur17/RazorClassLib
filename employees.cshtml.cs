230919d38b5ed7f436e90e6c33ab42774f5c847a
thakur17 <42466300+thakur17@users.noreply.github.com>
2023-05-19 03:31:56 -0400

    Add files via upload

---
+++ employees.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages; // PageModel
+using NorthwindEntitiesLib; // Employee
+using System.Linq; // ToArray()
+using System.Collections.Generic; // IEnumerable<T>
+
+
+namespace PacktFeatures.Pages
+
+{
+
+    public class EmployeesPageModel : PageModel
+    {
+        private Northwind db;
+        public EmployeesPageModel(Northwind injectedContext)
+        {
+            db = injectedContext;
+        }
+        public IEnumerable<Employee> Employees { get; set; }
+        public void OnGet()
+        {
+            Employees = db.Employees.ToArray();
+        }
+
+    }
+}
+
+
